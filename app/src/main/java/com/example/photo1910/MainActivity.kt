1cdc961d6c4d6e7e1768178dc61876a805cdbc13
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-10-19 10:17:20 +0200

    przycisk pozwala zobic zdj

--- app/src/main/java/com/example/photo1910/MainActivity.kt
+++ app/src/main/java/com/example/photo1910/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.photo1910
 
 
 
-
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +12,7 @@ import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var uploaded_image: ImageView
-    lateinit var btn_picture: Button
+
     val pick_image = 100
     var image_uri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,23 +20,23 @@ class MainActivity : AppCompatActivity() {
 
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-            title = "photo"
-            btn_picture = findViewById<Button>(R.id.btn_picture)
-            uploaded_image = findViewById<ImageView>(R.id.uploaded_image)
-            btn_picture.setOnClickListener(){
-                val picture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(picture, 200)
-            }
+            val button = findViewById<Button>(R.id.btn_picture)
+        button.setOnClickListener(){
+            val picture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(picture, 200)
+        }
 
 
 
-        setContentView(R.layout.activity_main)
+
+
+
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == RESULT_OK && requestCode == pick_image) {
             image_uri = data?.data
             uploaded_image.setImageURI(image_uri)
         }
-    }
+    }*/
 }
