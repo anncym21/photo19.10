82551bed46d99c20875c98a305348888c1c778fc
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-10-19 09:00:22 +0200

    manifest, przycisk i imageview

--- app/src/main/java/com/example/photo1910/MainActivity.kt
+++ app/src/main/java/com/example/photo1910/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.photo1910
 
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val image_btn = findViewById<Button>(R.id.btn_picture)
+        val image_view = findViewById<ImageView>(R.id.uploaded_image)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
