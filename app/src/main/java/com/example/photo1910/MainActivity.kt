807f5675f181524f9428228ea75353d0e0a64e60
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-10-19 10:29:44 +0200

    aplikacja pozwala zrobic zdjecie i je wyswietlic

--- app/src/main/java/com/example/photo1910/MainActivity.kt
+++ app/src/main/java/com/example/photo1910/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.photo1910
 
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,17 +14,20 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    val pick_image = 100
-    var image_uri: Uri? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+            val uploaded_image = findViewById<ImageView>(R.id.uploaded_image)
             val button = findViewById<Button>(R.id.btn_picture)
         button.setOnClickListener(){
+
             val picture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(picture, 200)
+            uploaded_image.setRotationX(99f)
         }
 
 
@@ -32,11 +36,11 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val uploaded_image = findViewById<ImageView>(R.id.uploaded_image)
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK && requestCode == pick_image) {
-            image_uri = data?.data
-            uploaded_image.setImageURI(image_uri)
+        if(resultCode == RESULT_OK && data != null) {
+            uploaded_image.setImageBitmap(data.extras?.get("data") as Bitmap)
         }
-    }*/
+    }
 }
