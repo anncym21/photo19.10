3781d05e7caa951dbe302b834933231082577158
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-10-21 10:18:51 +0200

    działająca aplikacja

--- app/src/main/java/com/example/photo1910/MainActivity.kt
+++ app/src/main/java/com/example/photo1910/MainActivity.kt
@@ -4,12 +4,15 @@ package com.example.photo1910
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.SeekBar
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,12 +26,124 @@ class MainActivity : AppCompatActivity() {
             setContentView(R.layout.activity_main)
             val uploaded_image = findViewById<ImageView>(R.id.uploaded_image)
             val button = findViewById<Button>(R.id.btn_picture)
+            val rotationX = findViewById<SeekBar>(R.id.seekBar_rotationX)
+            val rotationY = findViewById<SeekBar>(R.id.seekBar_rotationY)
+            val img_visibility = findViewById<SeekBar>(R.id.seekBar_visibility)
+            val red = findViewById<SeekBar>(R.id.seekBar_red)
+            val green = findViewById<SeekBar>(R.id.seekBar_green)
+            val blue = findViewById<SeekBar>(R.id.seekBar_blue)
+
+        fun set_rgb()
+        {
+            var colour = Color.rgb(red.progress,green.progress,blue.progress)
+            uploaded_image.setColorFilter(colour,PorterDuff.Mode.OVERLAY)
+        }
         button.setOnClickListener(){
 
             val picture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(picture, 200)
-            uploaded_image.setRotationX(99f)
+
         }
+        rotationX?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                uploaded_image.setRotationX(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+        rotationY?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                uploaded_image.setRotationY(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+        img_visibility?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                uploaded_image.setImageAlpha(progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+        red?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                set_rgb()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+        green?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                set_rgb()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+        blue?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                set_rgb()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+
+            }
+        })
+
+        /*rotation.setOnClickListener(){
+            uploaded_image.setRotationX(99f)
+        }*/
 
 
 
